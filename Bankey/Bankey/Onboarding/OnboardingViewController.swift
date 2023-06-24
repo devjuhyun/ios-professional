a3c67b5fa1d9b833de20415fd854202126953ac4
Juhyun Yun <juhyunyun@Juhyunui-Macmini.local>
2023-06-24 17:56:07 +0900

    refactor: create generic onboarding view controller
    This commit adds a generic onboarding view controller that can be reused
    across multiple onboarding screens.

--- Bankey/Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -12,16 +12,35 @@ class OnboardingViewController: UIViewController {
     let stackView = UIStackView()
     let imageView = UIImageView()
     let label = UILabel()
-    
+
+    let heroImageName: String
+    let titleText: String
+
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
     }
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    // 스토리보드가 뷰컨트롤러로 serialized 되는 코드? 뷰 컨트롤러를 코드로 만들때 꼭 넣어줘야된다.
+    required init?(coder: NSCoder) {
+        // 스토리보드를 쓰지 않기 때문에 이 이니셜라이저는 절대로 실행되서는 안된다.
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
 
 extension OnboardingViewController {
     func style() {
+        view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
@@ -29,7 +48,7 @@ extension OnboardingViewController {
         // Image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit // 이미지 뷰 안에 이미지 꽉 차게
-        imageView.image = UIImage(named: "delorean")
+        imageView.image = UIImage(named: heroImageName)
         
         // Label
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -37,7 +56,7 @@ extension OnboardingViewController {
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true // 사용 기기에 따라서 폰트 조정
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        label.text = titleText
     }
     
     func layout() {
