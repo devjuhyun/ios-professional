34af2398dbc3698a92a6dea1996481938ec43462
Juhyun Yun <juhyunyun@Juhyunui-Macmini.local>
2023-06-24 16:45:04 +0900

    Add first onboarding screen

---
+++ Bankey/Bankey/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,56 @@
+//
+//  OnboardingViewController.swift
+//  Bankey
+//
+//  Created by Juhyun Yun on 2023/06/24.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let imageView = UIImageView()
+    let label = UILabel()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    func style() {
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        // Image
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit // 이미지 뷰 안에 이미지 꽉 차게
+        imageView.image = UIImage(named: "delorean")
+        
+        // Label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true // 사용 기기에 따라서 폰트 조정
+        label.numberOfLines = 0
+        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
